01d1d05ce375df712c37d18757c43ed5a0512830
Mark Wicker <12748090+wickahead@users.noreply.github.com>
2023-12-18 16:46:42 -0800

    updated _slash_ js _slash_ config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "us-west-2_YeyBxZ3Bw", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "55mdb56gh8mjnlka6ks9qh48s2", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-west-2", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
